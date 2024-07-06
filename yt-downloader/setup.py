ab2d438a787fcb3c11450dbd96efabf938a1d5a7
Aryankpoor <Kapooraryan0419@gmail.com>
2024-07-07 00:43:50 +0530

    added yt downloader

---
+++ yt-downloader/setup.py
@@ -0,0 +1,13 @@
+from cx_Freeze import setup, Executable
+
+setup(
+
+       name="YT-DOWNLOADER",
+
+       version="1.0",
+
+       description="Download any youtube video",
+
+       executables=[Executable("your_script.py")],
+
+   )   
