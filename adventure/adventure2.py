8ee32c3f3bc97db46abd7ebc4a6f5ffdbbc76336
Aryankpoor <Kapooraryan0419@gmail.com>
2024-07-08 22:50:44 +0530

    added adventure part 2

---
+++ adventure/adventure2.py
@@ -0,0 +1,5 @@
+name = input("Type your name: ")
+print("Welcome", name, "to the new version of adventure!")
+
+answer = input(
+    "You are on a , it has come to an end and you can go left or right. Which way would you like to go? ").lower()
