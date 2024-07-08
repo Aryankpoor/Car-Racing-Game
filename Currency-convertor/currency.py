b98c1f32ed9b4a9e265930844eedfe7660505866
Aryankpoor <Kapooraryan0419@gmail.com>
2024-07-08 22:54:21 +0530

    removed dollar in currency converter to change to rupess

--- Currency-convertor/currency.py
+++ Currency-convertor/currency.py
@@ -1,10 +1,7 @@
 def main():
-    print("This program converts US dollars to Pounds Sterling")
+    print("")
     print()
 
-    dollars = eval(input("Enter amount in dollars: "))
-
-    pounds = convert_to_pounds(dollars)
 
     print("That is" , pounds, "pounds. ")
 
--- adventure/adventure2.py
+++ adventure/adventure2.py
@@ -3,3 +3,6 @@ print("Welcome", name, "to the new version of adventure!")
 
 answer = input(
     "You are on a , it has come to an end and you can go left or right. Which way would you like to go? ").lower()
+
+if answer == "left":
+    way = input("You come towards a dead end, but there is a hidden path to the left. Do you want to take it? (yes/no) ")
