81d19032b766b357c6beda9402c05217d71adaba
Aryan Kapoor <64773763+Aryankpoor@users.noreply.github.com>
2024-07-12 16:35:01 +0530

    Create audio-extractor.py

---
+++ audio-extractor.py
@@ -0,0 +1,13 @@
+# This program extracts audio from any video
+
+import moviepy.editor 
+
+
+# Video path
+video = moviepy.editor.VideoFileClip('file.mp4')
+
+
+#audio conversion
+audio = video.audio
+
+audio.write_audiofile('file.mp3')
